b3ac17d580f3b7f084c07be171e642ab225ece00
owennguyen3162 <nguyentuan3162@gmail.com>
2025-05-14 23:14:17 +0700

    config

---
+++ PhotoCleanEasy/Core/Base/BaseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Untitled.swift
+//  PhotoCleanEasy
+//
+//  Created by Nguyen Anh Tuan on 14/5/25.
+//
+
+
+import UIKit
+
+protocol IViewController {
+    func unmount()
+    func initUI()
+    func applicationDidBecomeActive()
+    func applicationDidEnterBackground()
+}
+
+class BaseViewController: UIViewController, IViewController {
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        initUI()
+    }
+    
+    deinit {
+        unmount()
+    }
+    
+    
+    func unmount() {}
+    func initUI() {}
+    func applicationDidBecomeActive(){}
+    func applicationDidEnterBackground(){}
+    
+}
---
+++ PhotoCleanEasy/Views/CleanerViewController/Views/CleanerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CleanerViewController.swift
+//  PhotoCleanEasy
+//
+//  Created by Nguyen Anh Tuan on 14/5/25.
+//
+
+import UIKit
+
+class CleanerViewController: BaseViewController {
+    
+    override func initUI() {
+        super.initUI()
+        
+        self.view.backgroundColor = UIColor(hex: "#141414")
+    }
+    
+    override func unmount() {
+        super.unmount()
+    }
+
+}
